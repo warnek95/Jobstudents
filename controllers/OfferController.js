b2acab8f1c2d9d8e0eb400748c4192ccfe3fd04c
aclement54 <alexandre.clement94@gmail.com>
2016-03-15 11:12:03 +0100

    Recherche d'offre dans le controleur
    Ne marche pas encore

--- controllers/OfferController.js
+++ controllers/OfferController.js
@@ -5,5 +5,14 @@ module.exports = {
     Offer.create(req.body,function(){
     next();
     })
-  }
-};
+  function getAllOffer(res,next){
+    Offer.findOne(function(err, user){
+      console.log(offer);
+        if(offer) return console.log(err);
+        if(offer){
+          next(offer);
+        }else {
+          res.redirect('/')
+        }
+      });
+    }
